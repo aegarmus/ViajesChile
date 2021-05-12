ecfafdef5fdba2928e42bfde0c54f4d19729d1eb
Alan Garcia Muñoz <aegarmus@gmail.com>
2021-05-12 06:51:59 -0400

    incorporación de fuente Raleway y ajuste de pesos. Se agrego tooltip en los iconos de rrss

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 $(function(){
 
-    $("a").click(function(event){
+    $("a").click(function(event){   //Definimos selector "a" con evento click para realizar una función
       if (this.hash !== "") {
-        event.preventDefault();
+        event.preventDefault();     //evitamos el evento default de desplazar por la página  
   
-        var gato = this.hash;
+        var gato = this.hash;       //Definimos nuestra variable con hash (#)
   
         $("html, body").animate({
-          scrollTop: $(gato).offset().top
-        }, 800, function(){
+          scrollTop: $(gato).offset().top //Definimos realizar un scroll hacia la variable definida
+        }, 800, function(){               // definimos la velocidad de desplazamiento y hacia donde
           window.location.hash = gato;
         });
       }
     });
+
+    $('[data-toggle="popover"]').popover(); //definimos el evento el selector [data-toggle="popover"]
 })
